a5a3f6107e3b01136a73bf4655b1851f528f99bc
zanlai <zanlai.hu@student.unsw.edu.au>
2021-06-07 21:57:09 +0800

    1

---
+++ main.js
@@ -0,0 +1,13 @@
+// hide
+// $(document).ready(function(){
+//     $("p").click(function(){
+//         $(this).hide();
+//     });
+// });
+
+// hide
+$(function(){
+    $("p").click(function(){
+        $("h1").hide();
+    });
+});
